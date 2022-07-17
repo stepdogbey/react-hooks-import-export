b2a6fcda1fc3fe3167a375f9840382463c40f897
stephen <stevedogbey@gmail.com>
2022-07-17 20:23:31 +0000

    completed

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,21 @@
 import React from "react";
 import howManyParks from "./parks/howManyParks";
+import * as RMFunctions from "./parks/RockyMountain";
+import MesaVerde from "./parks/MesaVerde";
 
 function ColoradoStateParks() {
   howManyParks(); // => "42 parks!"
+  MesaVerde();
+  RMFunctions.wildlife();
+  console.log(RMFunctions.trees);
+  RMFunctions.elevation();
 
-  return <h1>Colorado State Parks!</h1>;
+  return (
+    <div>
+      <MesaVerde />
+      <h1>Colorado State Parks!</h1>
+    </div>
+  );
 }
+
+export default ColoradoStateParks;
